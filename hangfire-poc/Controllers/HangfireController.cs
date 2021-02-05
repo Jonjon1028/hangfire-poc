e27b7d4f303dd4cd6b14e7b20350da99759ea714
Jon <apacojr@gmail.com>
2021-02-05 14:35:14 +0800

    Add project files.

---
+++ hangfire-poc/Controllers/HangfireController.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hangfire_poc.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+
+        public IActionResult hangfireJob()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendEmail("POC"));
+
+            return Ok($"Job Id: {jobId} | POC Success");
+        }
+
+        public void SendEmail(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
